b5b50d12a5d67ae972d1afc2c9da8ddc809f6d9a
emorozov <zhenya.morozov@gmail.com>
2024-10-02 16:09:17 +0400

    first commit

---
+++ triangles.py
@@ -0,0 +1,10 @@
+
+def is_triangle(a, b, c):
+    '''Returns True if three sides can form a valid triangle'''
+    sides = [a, b, c]
+    sides.sort()
+    pass
+    # if sides[]
+
+# this will print
+print(is_triangle(3, 5, 4))
